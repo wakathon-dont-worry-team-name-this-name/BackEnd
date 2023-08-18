ff72c3bddaddbca73e46ac88e77a9fa9c882a790
Gihyun <rlgus2738@naver.com>
2023-08-18 21:46:17 +0900

    m

--- src/main/java/com/project/wakathon/Gatwork/BackEnd/Member/controller/MemberController.java
+++ src/main/java/com/project/wakathon/Gatwork/BackEnd/Member/controller/MemberController.java
@@ -1,7 +1,12 @@
 package com.project.wakathon.Gatwork.BackEnd.Member.controller;
 
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpResponse;
 
 @RestController
 public class MemberController {
