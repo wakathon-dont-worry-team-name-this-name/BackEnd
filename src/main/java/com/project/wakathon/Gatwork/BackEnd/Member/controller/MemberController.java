4d7a816077e68911743ae95ee2c20f2363610176
Gihyun <rlgus2738@naver.com>
2023-08-19 00:33:25 +0900

    feat: 선택한 명함 카드 가져오기, Tag 리스트 가져오기

--- src/main/java/com/project/wakathon/Gatwork/BackEnd/Member/controller/MemberController.java
+++ src/main/java/com/project/wakathon/Gatwork/BackEnd/Member/controller/MemberController.java
@@ -1,14 +1,30 @@
 package com.project.wakathon.Gatwork.BackEnd.Member.controller;
 
-import lombok.Getter;
-import org.springframework.stereotype.Controller;
+import com.project.wakathon.Gatwork.BackEnd.Member.response.CardResponseDto;
+import com.project.wakathon.Gatwork.BackEnd.Member.response.TagsInUserResponseDto;
+import com.project.wakathon.Gatwork.BackEnd.Member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpResponse;
-
 @RestController
+@RequiredArgsConstructor
 public class MemberController {
 
+    private final MemberService memberService;
+
+    // 태그 정보 받기
+    @GetMapping(path = "api/member/{memberId}/tags")
+    public TagsInUserResponseDto getTagsInUser(@PathVariable int memberId) {
+        return memberService.getTagsInUser(memberId);
+    }
+
+    // 추천을 받은 명함 리스트 보내기
+
+    // 선택한 명함의 정보를 받기
+    @GetMapping(path = "api/member/{memberId}")
+    public CardResponseDto getCard(@PathVariable int memberId) {
+        return memberService.getCard(memberId);
+    }
 }
--- src/main/java/com/project/wakathon/Gatwork/BackEnd/Member/service/MemberService.java
+++ src/main/java/com/project/wakathon/Gatwork/BackEnd/Member/service/MemberService.java
@@ -1,9 +1,59 @@
 package com.project.wakathon.Gatwork.BackEnd.Member.service;
 
-import io.swagger.v3.oas.annotations.servers.Server;
+import com.project.wakathon.Gatwork.BackEnd.Member.Repository.CardRepository;
+import com.project.wakathon.Gatwork.BackEnd.Member.Repository.MemberRepository;
+import com.project.wakathon.Gatwork.BackEnd.Member.Repository.MemberTagRepository;
+import com.project.wakathon.Gatwork.BackEnd.Member.Repository.TagRepository;
+import com.project.wakathon.Gatwork.BackEnd.Member.domain.Card;
+import com.project.wakathon.Gatwork.BackEnd.Member.domain.Member;
+import com.project.wakathon.Gatwork.BackEnd.Member.domain.Tag;
+import com.project.wakathon.Gatwork.BackEnd.Member.domain.TagCategory;
+import com.project.wakathon.Gatwork.BackEnd.Member.response.CardResponseDto;
+import com.project.wakathon.Gatwork.BackEnd.Member.response.TagsInUserResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-public class MemberService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final MemberTagRepository memberTagRepository;
+    private final TagRepository tagRepository;
+    private final CardRepository cardRepository;
+
+
+    // Tag 리스트
+    public TagsInUserResponseDto getTagsInUser(int memberId) {
+        // Client Error Handling
+        List<Tag> tags = memberTagRepository.CustomTagFindByMemberId(memberId);
+
+        // todo: 유효성 검사
+
+
+        return TagsInUserResponseDto.builder()
+                .tagCategories((ArrayList<TagCategory>) tags.stream()
+                        .map(Tag::getTagCategory)
+                        .distinct()
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+    // 선택된 명함 카드 가져오기
+    public CardResponseDto getCard(int memberId) {
+        Card card = cardRepository.findById(memberId).orElseThrow(RuntimeException::new);
+        Member member = memberRepository.findById(memberId).orElseThrow(RuntimeException::new);
+
+        return CardResponseDto.builder()
+                .studentId(member.getStudentId())
+                .profileImg(member.getProfileImg())
+                .viewCount(card.getViewCount())
+                .score(card.getScore())
+                .major(card.getMajor())
+                .introduce(member.getIntroduce())
+                .build();
+    }
 }
