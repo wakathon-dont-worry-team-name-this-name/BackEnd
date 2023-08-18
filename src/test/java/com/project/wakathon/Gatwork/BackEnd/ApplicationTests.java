37ad019534d80c11a64520eff56bcad504b388e8
ydg <ydg983@naver.com>
2023-08-18 15:27:56 +0900

    first commit

---
+++ src/test/java/com/project/wakathon/Gatwork/BackEnd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wakathon.Gatwork.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
